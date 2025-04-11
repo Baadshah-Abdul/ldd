40afaa3f4d7e402152b3e836e16158e19ad7ba2e
Baadshah-Abdul <abdulhaque1732@gmail.com>
2025-04-11 10:45:56 -0700

    file rm

--- example/.example.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/ldd_rpi/ldd/example/example.mod := printf '%s\n'   example.o | awk '!x[$$0]++ { print("/home/ldd_rpi/ldd/example/"$$0) }' > /home/ldd_rpi/ldd/example/example.mod
--- example/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/ldd_rpi/ldd/example/modules.order := {   echo /home/ldd_rpi/ldd/example/example.ko; :; } > /home/ldd_rpi/ldd/example/modules.order
