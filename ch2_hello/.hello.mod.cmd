7012518f2eb42c9f63ed95240f44b1e20dee8d69
Baadshah-Abdul <abdulhaque1732@gmail.com>
2025-04-11 10:52:17 -0700

    file rm

--- ch2_hello/.hello.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/ldd_rpi/ldd/hello/hello.mod := printf '%s\n'   hello.o | awk '!x[$$0]++ { print("/home/ldd_rpi/ldd/hello/"$$0) }' > /home/ldd_rpi/ldd/hello/hello.mod
--- ch2_hello/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/ldd_rpi/ldd/hello/modules.order := {   echo /home/ldd_rpi/ldd/hello/hello.ko; :; } > /home/ldd_rpi/ldd/hello/modules.order
